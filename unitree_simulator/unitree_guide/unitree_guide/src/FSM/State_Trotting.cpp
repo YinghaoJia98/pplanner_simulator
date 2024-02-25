db30ec7e82b889dd230281f8acf2619302c8a6e7
YinghaoJia <yinghaojia@163.com>
2024-02-25 21:22:00 +0800

    20240225

--- unitree_simulator/unitree_guide/unitree_guide/src/FSM/State_Trotting.cpp
+++ unitree_simulator/unitree_guide/unitree_guide/src/FSM/State_Trotting.cpp
@@ -5,18 +5,19 @@
 #include <iomanip>
 
 State_Trotting::State_Trotting(CtrlComponents *ctrlComp)
-             :FSMState(ctrlComp, FSMStateName::TROTTING, "trotting"), 
-              _est(ctrlComp->estimator), _phase(ctrlComp->phase), 
-              _contact(ctrlComp->contact), _robModel(ctrlComp->robotModel), 
-              _balCtrl(ctrlComp->balCtrl){
+    : FSMState(ctrlComp, FSMStateName::TROTTING, "trotting"),
+      _est(ctrlComp->estimator), _phase(ctrlComp->phase),
+      _contact(ctrlComp->contact), _robModel(ctrlComp->robotModel),
+      _balCtrl(ctrlComp->balCtrl)
+{
     _gait = new GaitGenerator(ctrlComp);
 
-    _gaitHeight = 0.08;
+    _gaitHeight = 0.18;
 
 #ifdef ROBOT_TYPE_Go1
     _Kpp = Vec3(70, 70, 70).asDiagonal();
     _Kdp = Vec3(10, 10, 10).asDiagonal();
-    _kpw = 780; 
+    _kpw = 780;
     _Kdw = Vec3(70, 70, 70).asDiagonal();
     _KpSwing = Vec3(400, 400, 400).asDiagonal();
     _KdSwing = Vec3(10, 10, 10).asDiagonal();
@@ -34,14 +35,15 @@ State_Trotting::State_Trotting(CtrlComponents *ctrlComp)
     _vxLim = _robModel->getRobVelLimitX();
     _vyLim = _robModel->getRobVelLimitY();
     _wyawLim = _robModel->getRobVelLimitYaw();
-
 }
 
-State_Trotting::~State_Trotting(){
+State_Trotting::~State_Trotting()
+{
     delete _gait;
 }
 
-void State_Trotting::enter(){
+void State_Trotting::enter()
+{
     _pcd = _est->getPosition();
     _pcd(2) = -_robModel->getFeetPosIdeal()(2, 0);
     _vCmdBody.setZero();
@@ -53,24 +55,30 @@ void State_Trotting::enter(){
     _gait->restart();
 }
 
-void State_Trotting::exit(){
+void State_Trotting::exit()
+{
     _ctrlComp->ioInter->zeroCmdPanel();
     _ctrlComp->setAllSwing();
 }
 
-FSMStateName State_Trotting::checkChange(){
-    if(_lowState->userCmd == UserCommand::L2_B){
+FSMStateName State_Trotting::checkChange()
+{
+    if (_lowState->userCmd == UserCommand::L2_B)
+    {
         return FSMStateName::PASSIVE;
     }
-    else if(_lowState->userCmd == UserCommand::L2_A){
+    else if (_lowState->userCmd == UserCommand::L2_A)
+    {
         return FSMStateName::FIXEDSTAND;
     }
-    else{
+    else
+    {
         return FSMStateName::TROTTING;
     }
 }
 
-void State_Trotting::run(){
+void State_Trotting::run()
+{
     _posBody = _est->getPosition();
     _velBody = _est->getVelocity();
     _posFeet2BGlobal = _est->getPosFeet2BGlobal();
@@ -86,15 +94,18 @@ void State_Trotting::run(){
     getUserCmd();
     calcCmd();
 
-    _gait->setGait(_vCmdGlobal.segment(0,2), _wCmdGlobal(2), _gaitHeight);
+    _gait->setGait(_vCmdGlobal.segment(0, 2), _wCmdGlobal(2), _gaitHeight);
     _gait->run(_posFeetGlobalGoal, _velFeetGlobalGoal);
 
     calcTau();
     calcQQd();
 
-    if(checkStepOrNot()){
+    if (checkStepOrNot())
+    {
         _ctrlComp->setStartWave();
-    }else{
+    }
+    else
+    {
         _ctrlComp->setAllStance();
     }
 
@@ -102,55 +113,65 @@ void State_Trotting::run(){
     _lowCmd->setQ(vec34ToVec12(_qGoal));
     _lowCmd->setQd(vec34ToVec12(_qdGoal));
 
-    for(int i(0); i<4; ++i){
-        if((*_contact)(i) == 0){
+    for (int i(0); i < 4; ++i)
+    {
+        if ((*_contact)(i) == 0)
+        {
             _lowCmd->setSwingGain(i);
-        }else{
+        }
+        else
+        {
             _lowCmd->setStableGain(i);
         }
     }
-
 }
 
-bool State_Trotting::checkStepOrNot(){
-    if( (fabs(_vCmdBody(0)) > 0.03) ||
+bool State_Trotting::checkStepOrNot()
+{
+    if ((fabs(_vCmdBody(0)) > 0.03) ||
         (fabs(_vCmdBody(1)) > 0.03) ||
         (fabs(_posError(0)) > 0.08) ||
         (fabs(_posError(1)) > 0.08) ||
         (fabs(_velError(0)) > 0.05) ||
         (fabs(_velError(1)) > 0.05) ||
-        (fabs(_dYawCmd) > 0.20) ){
+        (fabs(_dYawCmd) > 0.20))
+    {
         return true;
-    }else{
+    }
+    else
+    {
         return false;
     }
 }
 
-void State_Trotting::setHighCmd(double vx, double vy, double wz){
+void State_Trotting::setHighCmd(double vx, double vy, double wz)
+{
     _vCmdBody(0) = vx;
     _vCmdBody(1) = vy;
-    _vCmdBody(2) = 0; 
+    _vCmdBody(2) = 0;
     _dYawCmd = wz;
 }
 
-void State_Trotting::getUserCmd(){
+void State_Trotting::getUserCmd()
+{
     /* Movement */
-    _vCmdBody(0) =  invNormalize(_userValue.ly, _vxLim(0), _vxLim(1));
+    _vCmdBody(0) = invNormalize(_userValue.ly, _vxLim(0), _vxLim(1));
     _vCmdBody(1) = -invNormalize(_userValue.lx, _vyLim(0), _vyLim(1));
     _vCmdBody(2) = 0;
 
     /* Turning */
     _dYawCmd = -invNormalize(_userValue.rx, _wyawLim(0), _wyawLim(1));
-    _dYawCmd = 0.9*_dYawCmdPast + (1-0.9) * _dYawCmd;
+    _dYawCmd = 0.9 * _dYawCmdPast + (1 - 0.9) * _dYawCmd;
     _dYawCmdPast = _dYawCmd;
 }
 
-void State_Trotting::calcCmd(){
+void State_Trotting::calcCmd()
+{
     /* Movement */
     _vCmdGlobal = _B2G_RotMat * _vCmdBody;
 
-    _vCmdGlobal(0) = saturation(_vCmdGlobal(0), Vec2(_velBody(0)-0.2, _velBody(0)+0.2));
-    _vCmdGlobal(1) = saturation(_vCmdGlobal(1), Vec2(_velBody(1)-0.2, _velBody(1)+0.2));
+    _vCmdGlobal(0) = saturation(_vCmdGlobal(0), Vec2(_velBody(0) - 0.2, _velBody(0) + 0.2));
+    _vCmdGlobal(1) = saturation(_vCmdGlobal(1), Vec2(_velBody(1) - 0.2, _velBody(1) + 0.2));
 
     _pcd(0) = saturation(_pcd(0) + _vCmdGlobal(0) * _ctrlComp->dt, Vec2(_posBody(0) - 0.05, _posBody(0) + 0.05));
     _pcd(1) = saturation(_pcd(1) + _vCmdGlobal(1) * _ctrlComp->dt, Vec2(_posBody(1) - 0.05, _posBody(1) + 0.05));
@@ -164,12 +185,13 @@ void State_Trotting::calcCmd(){
     _wCmdGlobal(2) = _dYawCmd;
 }
 
-void State_Trotting::calcTau(){
+void State_Trotting::calcTau()
+{
     _posError = _pcd - _posBody;
     _velError = _vCmdGlobal - _velBody;
 
     _ddPcd = _Kpp * _posError + _Kdp * _velError;
-    _dWbd  = _kpw*rotMatToExp(_Rd*_G2B_RotMat) + _Kdw * (_wCmdGlobal - _lowState->getGyroGlobal());
+    _dWbd = _kpw * rotMatToExp(_Rd * _G2B_RotMat) + _Kdw * (_wCmdGlobal - _lowState->getGyroGlobal());
 
     _ddPcd(0) = saturation(_ddPcd(0), Vec2(-3, 3));
     _ddPcd(1) = saturation(_ddPcd(1), Vec2(-3, 3));
@@ -179,11 +201,13 @@ void State_Trotting::calcTau(){
     _dWbd(1) = saturation(_dWbd(1), Vec2(-40, 40));
     _dWbd(2) = saturation(_dWbd(2), Vec2(-10, 10));
 
-    _forceFeetGlobal = - _balCtrl->calF(_ddPcd, _dWbd, _B2G_RotMat, _posFeet2BGlobal, *_contact);
+    _forceFeetGlobal = -_balCtrl->calF(_ddPcd, _dWbd, _B2G_RotMat, _posFeet2BGlobal, *_contact);
 
-    for(int i(0); i<4; ++i){
-        if((*_contact)(i) == 0){
-            _forceFeetGlobal.col(i) = _KpSwing*(_posFeetGlobalGoal.col(i) - _posFeetGlobal.col(i)) + _KdSwing*(_velFeetGlobalGoal.col(i)-_velFeetGlobal.col(i));
+    for (int i(0); i < 4; ++i)
+    {
+        if ((*_contact)(i) == 0)
+        {
+            _forceFeetGlobal.col(i) = _KpSwing * (_posFeetGlobalGoal.col(i) - _posFeetGlobal.col(i)) + _KdSwing * (_velFeetGlobalGoal.col(i) - _velFeetGlobal.col(i));
         }
     }
 
@@ -192,18 +216,19 @@ void State_Trotting::calcTau(){
     _tau = _robModel->getTau(_q, _forceFeetBody);
 }
 
-void State_Trotting::calcQQd(){
+void State_Trotting::calcQQd()
+{
 
     Vec34 _posFeet2B;
-    _posFeet2B = _robModel->getFeet2BPositions(*_lowState,FrameType::BODY);
-    
-    for(int i(0); i<4; ++i){
+    _posFeet2B = _robModel->getFeet2BPositions(*_lowState, FrameType::BODY);
+
+    for (int i(0); i < 4; ++i)
+    {
         _posFeet2BGoal.col(i) = _G2B_RotMat * (_posFeetGlobalGoal.col(i) - _posBody);
-        _velFeet2BGoal.col(i) = _G2B_RotMat * (_velFeetGlobalGoal.col(i) - _velBody); 
-        // _velFeet2BGoal.col(i) = _G2B_RotMat * (_velFeetGlobalGoal.col(i) - _velBody - _B2G_RotMat * (skew(_lowState->getGyro()) * _posFeet2B.col(i)) );  //  c.f formula (6.12) 
+        _velFeet2BGoal.col(i) = _G2B_RotMat * (_velFeetGlobalGoal.col(i) - _velBody);
+        // _velFeet2BGoal.col(i) = _G2B_RotMat * (_velFeetGlobalGoal.col(i) - _velBody - _B2G_RotMat * (skew(_lowState->getGyro()) * _posFeet2B.col(i)) );  //  c.f formula (6.12)
     }
-    
+
     _qGoal = vec12ToVec34(_robModel->getQ(_posFeet2BGoal, FrameType::BODY));
     _qdGoal = vec12ToVec34(_robModel->getQd(_posFeet2B, _velFeet2BGoal, FrameType::BODY));
 }
-

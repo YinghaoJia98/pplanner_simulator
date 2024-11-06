ae77d68c720e9b9db28be763a81078fa1cffbcb5
YinghaoJia <yinghaojia@163.com>
2024-11-06 18:03:41 +0800

    Revise UnitreeA1 Sensor Head Model. 20241106.

--- unitree_simulator/a1_cpp/src/A1Params.h
+++ unitree_simulator/a1_cpp/src/A1Params.h
@@ -7,7 +7,7 @@
 
 // control time related
 // #define CTRL_FREQUENCY 2.5  // ms
-#define GRF_UPDATE_FREQUENCY 2.0        // ms
+#define GRF_UPDATE_FREQUENCY 2.0        // ms 1.0 might work better
 #define MAIN_UPDATE_FREQUENCY 2.0       // ms
 #define HARDWARE_FEEDBACK_FREQUENCY 2.0 // ms
 
